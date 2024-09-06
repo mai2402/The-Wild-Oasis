6375a2a1b4dfd5cbb410e627131da4d64fca7134
mai2402 <maifareed242@gmail.com>
2024-09-06 15:55:27 +0300

    fixed a bug realted to pagination by setting the default page to 1

--- src/features/cabins/useURL.js
+++ src/features/cabins/useURL.js
@@ -6,6 +6,7 @@ import { useSearchParams } from "react-router-dom";
    
     function handleChange(value){
          searchParams.set(filterField,value)
+         if (searchParams.get("page")) searchParams.set("page",1) 
          setSearchParams(searchParams);
     }
 
