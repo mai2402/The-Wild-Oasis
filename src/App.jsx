e52550b412cfb168238b76c3666b227f81f6a00b
mai2402 <maifareed242@gmail.com>
2024-09-20 13:12:08 +0300

    added dark mode feature

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Toaster } from "react-hot-toast"
 import Booking from "./pages/Booking"
 import Checkin from "./pages/Checkin"
 import ProtectedRoute from "./ui/ProtectedRoute"
-
+import {DarkModeProvider} from "./contexts/darkModeContext"
 
 
 const queryClient =new QueryClient(
@@ -30,6 +30,8 @@ const queryClient =new QueryClient(
 
   function App() {
   return (
+    <DarkModeProvider>
+
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false}/>
       <GlobalStyles/>
@@ -58,24 +60,25 @@ const queryClient =new QueryClient(
            gutter={12}
            containerStyle={{margin:"8px"}}
            toastOptions={
-            {
-              success:{
-                durationL:3000,
-              },
-              error:{
-                duration:3000,
-              },
-              style :{
-                fontSize:"16px",
-                maxWidth :"500px",
-                padding:"16px 24px",
-                backgroundColor:"var(--color-grey-0)",
-                color:"var(--color-grey-700",
-
+             {
+               success:{
+                 durationL:3000,
+                },
+                error:{
+                  duration:3000,
+                },
+                style :{
+                  fontSize:"16px",
+                  maxWidth :"500px",
+                  padding:"16px 24px",
+                  backgroundColor:"var(--color-grey-0)",
+                  color:"var(--color-grey-700",
+                  
+                }
               }
-            }
-           }/>
+            }/>
     </QueryClientProvider>
+            </DarkModeProvider>
   )
 }
 
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import Logout from "../features/authentication/Logout"
 import ButtonIcon from "./ButtonIcon"
 import { HiOutlineUser } from "react-icons/hi2"
 import { useNavigate } from "react-router-dom"
+import DarkmodeToggle from "./DarkmodeToggle"
 
 const StyledHeader = styled.ul`
     display: flex;
@@ -19,6 +20,9 @@ function HeaderMenu() {
                 <ButtonIcon onClick={()=>navigate("/account")}>
                     <HiOutlineUser/>
                 </ButtonIcon>
+            </li>
+            <li>
+                <DarkmodeToggle/>
             </li>
             <li>
                 <Logout/>
